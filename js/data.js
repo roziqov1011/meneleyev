b94e696cc078e638d1183afbf68d2d58c769c25f
Roziboy <roziqov1011@gmail.com>
2023-07-24 09:38:40 +0500

    inital commit

---
+++ js/data.js
@@ -0,0 +1,33 @@
+const data = [
+    {
+        id:1,
+        name: 'Hydrogen',
+        ic: 'H',
+        tr:'1',
+        korinishi:"hidsiz, taʻmsiz va rangsiz gaz",
+        am:"1,00794",
+        mm:"m. a. b. (g/mol)",
+        ar:"79 pm",
+        ie:"1311,3 kJ/mol",
+        be:"(eV)",
+        ek:"1s,1",
+        kr:"32 pm",
+        od:"1, −1",
+        zich: "0,0708(-253 °C da) g/sm³",
+        shis:"14,267 J/(K·mol)",
+        io:"0,1815 Vt/(m·K)",
+        eh:"14,01 K",
+        qh:"20,28 K",
+        mh:"14,1 sm³/mol",
+        pt: "geksagonal",
+        dic:`Vodorod (lotincha: Hydrogenium), H — Kimyoviy elementlar davriy sistemasining birinchi guruhiga mansub kimyoviy element; atom massasi 1,00797. Odatdagi sharoitda rangsiz, hidsiz gaz boʻlib, taʼmi yoʻq. Metallarga kislotalar taʼsir ettirilganida yonuvchi gaz ajralishi 16- va 17-asr kimyogarlarining asarlarida koʻp eslatib oʻtiladi. 1766-yilda ingliz kimyogari Vodorod Kavendish ajralayotgan gazni yigʻib tekshirib, "alangalanuvchi havo" deb atadi va ana shu gaz flogiston boʻlsa kerak, deb oʻyladi. 1783-yilda Vodorod Lavuaze suvni analiz va sintez qilish yoʻli bilan uning tarkibi murakkab ekanini isbot etdi. 1787-yilda esa "alangalanuvchi gaz"ni yangi element deb taʼrifladi va unga hoz. hydrogene (yun. hydro—suv va gennao—tugʻdiraman) nomini berdi. Ruscha "Vodorod" nomini 1824-yilda Vodorod Vodorod Solovev taklif etgan.
+
+        Vodorod tabiatda keng tarqalgan boʻlib, Yer poʻsti (litosfera va gidrosfera) dagi miqdori ogʻirligi boʻyicha 1%, atomlar soni boʻyicha 16% ni tashkil etadi. Yerda eng koʻp tarqalgan birikma — suv (ogʻirligi boʻyicha 11,19% Vodorod) tarkibida hamda koʻmir, neft, tabiiy gazlar, tuproq, shuningdek, hayvon va oʻsimlik organizmlari (yaʼni oqsillar, nuklein kislotalar, yogʻlar, uglevodlar va b.) tarkibida uchraydi. Vodorod erkin holda juda kam, tabiiy gazlarda ozgina, atmosferada juda oz boʻladi. Yer atrofida protonlar oqimi sifatida Yerning ichki ("proton") radiatsion mintaqasini hosil qiladi. Fazoda eng koʻp tarqalgan element hisoblanadi. Plazma sifatida Quyosh va koʻpchilik yulduzlar ogʻirligining yarmini, yulduzlararo muhit hamda gaz tumanliklarining asosiy qismini tashkil etadi. Bir qator sayyoralarda N2 sifatida erkin hodda, metan SN4, ammiak NH3, suv N2O, =SN, =NH, —ON, =SiH, =RN va b. radikallar sifatida uchraydi.
+        
+        Oddiy Vodorod ikki barqaror izotop: yengil Vodorod yoki protiy (ʻN) va ogʻir Vodorod yoki deyteriy (2N yoki D) lardan iborat. Tabiiy birikmalarida bir atom 2N ga oʻrtacha 6800 ta ʻN toʻgʻri keladi. Radioaktiv izotopi — oʻta ogʻir Vodorod yoki tritiy (3N yoki T) sunʼiy yoʻl bilan olingan. Buning yarim yemirilish davri T1/2=12,262 y. Tabiatda tritiy kosmik nurlar neytronlarining taʼsirida azotdan hosil boʻladi. Juda beqaror izotopi 4N ham olingan. Vodorod atomi boshqa elementlarinikiga nisbatan oddiy boʻlib, yadro va bitta elektrondan iborat. Elektronning yadro bilan bogʻlanish energiyasi (ionizatsiya potensiali) 13,595 eV ga teng .
+        
+        Vodorod maʼlum moddalar ichida eng yengili boʻlib (havodan 14,5 baravar yengil), zichligi 0,0899 g/l (0° va 1 atm). Vodorod — 252,6° da qaynaydi (suyuqlanadi) va — 259,1° da suyuqlanadi (qotadi). Barcha gazlar ichida Vodorod eng koʻp issiq oʻtkazuvchandir (0° va 1 atmda 4,16-10 -* kal/sm sek. grad). Vodorod suvda kam eriydi (20° va 1 atmda 0,0182 ml/g), lekin metallar (Ni, Pt, Pd va b.) da, ayniqsa palladiyda yaxshi eriydi.
+        
+        Oddiy sharoitda molekula holidagi Vodorod faol emas, bunday holda faqat metallmaslar bilan (ftor, nur taʼsirida xlor bilan ham) birika oladi. Qizitilganda koʻpgina elementlar bilan birikadi. Atom holidagi Vodorod molekula holidagi Vodorodga nisbatan kimyoviy jihatdan juda faol hisoblanadi. Vodorod kislorod bilan suv hosil qiladi: N2+1/2O2=N2O; bunda 68,3174 kkal/mol issiqlik ajraladi (25° va 1 atm.). Odatdagi sharoitda reaksiya sekin, 550° dan yuqorida esa portlash bilan boradi. Vodorod-kislorod aralashmasining portlash chegaralari 4 dan 92% N2 gacha, vodorod-havo aralashmasiniki esa 4 dan 74% N2 gacha (2 hajm N2 va 1 hajm O, aralashmasi qaldiroq gaz deb ataladi). Vodorod oksidlardan kislorodni tortib ola olishi sababli koʻpgina metallarni qaytarish maqsadlarida ishlatiladi.`
+    },
+]
